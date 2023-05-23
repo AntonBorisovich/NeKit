ce8dda95b5ef62bf064bd368b4bae3e5d9a39fe5
Anton Borisovich <antosha969@gmail.com>
2023-05-23 22:31:41 +0300

    beep boop
    - [orgp] Добавлен todo лист, что бы не забыть как работает команда
    - [neuro] аааам. ну чето там поменялось, но не обращайте внимание

--- src/commands/neuro.js
+++ src/commands/neuro.js
@@ -2,11 +2,18 @@ const Discord = require("discord.js");
 const http = require("http");
 
 const PChostname = "192.168.10.67";
-const PCport = "7860";
-const defBatch = 1;
-const defSteps = 16;
-const width = 512;
-const height = 512;
+const PCport = "7861";
+const defaultSettings = {
+	batch: 1,
+	steps: 20,
+	sampler: "Euler a",
+	cfgScale: 7,
+	seed: -1,
+	width: 512,
+	height: 512,
+	denoising: 0.7,
+	clipSkip: 2
+}
 
 let workingNow = {timestamp: 0, id: false, step: 0};
 const boolToString = {
@@ -19,35 +26,77 @@ const secToMin = (seconds) => {
 	if (secs < 10) secs = "0" + secs;
 	return mins + ":" + secs;
 }
-const interruptTask = async (nek, client, interaction) => {
+let api = {};
+api.postInterrupt = async () => {
 	const options = { // параметры обращения к серваку
 		hostname: PChostname,
 		port: PCport,
 		path: '/sdapi/v1/interrupt',
 		method: 'POST',
-		headers: {}
+		headers: {'accept': 'application/json'}
 	}
 	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
 		const req = http.request(options, (res) => { // обращаемся к серваку
 			res.on('end', () => {
-				resolve(true)
+				resolve(true);
 			});
 		});
 		req.on('error', (e) => {
-			console.log(e)
+			console.error(e);
 			resolve(false);
 		});
 		req.end();
 	})
 }
-
-const checkProgress = async () => {
+api.postInterrogate = async () => {
 	const options = { // параметры обращения к серваку
 		hostname: PChostname,
 		port: PCport,
-		path: '/sdapi/v1/progress',
-		method: 'GET',
-		headers: {}
+		path: '/sdapi/v1/interrogate',
+		method: 'POST',
+		headers: {'accept': 'application/json', 'Content-Type': 'application/json'}
+	}
+	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
+		const req = http.request(options, (res) => { // обращаемся к серваку
+			res.on('end', () => {
+				resolve(true);
+			});
+		});
+		req.on('error', (e) => {
+			console.error(e);
+			resolve(false);
+		});
+		req.end();
+	})
+}
+api.postSkip = async () => {
+	const options = { // параметры обращения к серваку
+		hostname: PChostname,
+		port: PCport,
+		path: '/sdapi/v1/interrupt',
+		method: 'POST',
+		headers: {'accept': 'application/json'}
+	}
+	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
+		const req = http.request(options, (res) => { // обращаемся к серваку
+			res.on('end', () => {
+				resolve(true);
+			});
+		});
+		req.on('error', (e) => {
+			console.error(e);
+			resolve(false);
+		});
+		req.end();
+	})
+}
+api.postOptions = async (optionsObject = {}) => {
+	const options = { // параметры обращения к серваку
+		hostname: PChostname,
+		port: PCport,
+		path: '/sdapi/v1/options',
+		method: 'POST',
+		headers: {'accept': 'application/json', 'Content-Type': 'application/json'}
 	}
 	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
 		const req = http.request(options, (res) => { // обращаемся к серваку
@@ -57,7 +106,6 @@ const checkProgress = async () => {
 			});
 			res.on('end', () => {
 				try {
-					//console.log(data)
 					resolve(JSON.parse(data));
 				} catch(e) {
 					console.error(e);
@@ -66,7 +114,129 @@ const checkProgress = async () => {
 			});
 		});
 		req.on('error', (e) => {
-			console.log(e)
+			console.error(e);
+			resolve(false);
+		});
+		req.write(optionsObject); // пишем данные в сайт
+		req.end();
+	})
+	
+}
+api.getOptions = async () => {
+	const options = { // параметры обращения к серваку
+		hostname: PChostname,
+		port: PCport,
+		path: '/sdapi/v1/options',
+		method: 'GET',
+		headers: {'accept': 'application/json'}
+	}
+	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
+		const req = http.request(options, (res) => { // обращаемся к серваку
+			let data = "";
+			res.on('data', (d) => {
+				data = data + d;
+			});
+			res.on('end', () => {
+				try {
+					resolve(JSON.parse(data));
+				} catch(e) {
+					console.error(e);
+					resolve(false);
+				}
+			});
+		});
+		req.on('error', (e) => {
+			console.error(e);
+			resolve(false);
+		});
+		req.end();
+	})
+}
+api.getProgress = async (skip_current_image=false) => {
+	const options = { // параметры обращения к серваку
+		hostname: PChostname,
+		port: PCport,
+		path: '/sdapi/v1/progress?skip_current_image=' + skip_current_image,
+		method: 'GET',
+		headers: {'accept': 'application/json'}
+	}
+	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
+		const req = http.request(options, (res) => { // обращаемся к серваку
+			let data = "";
+			res.on('data', (d) => {
+				data = data + d;
+			});
+			res.on('end', () => {
+				try {
+					resolve(JSON.parse(data));
+				} catch(e) {
+					console.error(e);
+					resolve(false);
+				}
+			});
+		});
+		req.on('error', (e) => {
+			console.error(e)
+			resolve(false);
+		});
+		req.end();
+	})
+}
+api.getMemory = async () => {
+	const options = { // параметры обращения к серваку
+		hostname: PChostname,
+		port: PCport,
+		path: '/sdapi/v1/memory',
+		method: 'GET',
+		headers: {'accept': 'application/json'}
+	}
+	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
+		const req = http.request(options, (res) => { // обращаемся к серваку
+			let data = "";
+			res.on('data', (d) => {
+				data = data + d;
+			});
+			res.on('end', () => {
+				try {
+					resolve(JSON.parse(data));
+				} catch(e) {
+					console.error(e);
+					resolve(false);
+				}
+			});
+		});
+		req.on('error', (e) => {
+			console.error(e);
+			resolve(false);
+		});
+		req.end();
+	})
+}
+api.getSdModels = async () => {
+	const options = { // параметры обращения к серваку
+		hostname: PChostname,
+		port: PCport,
+		path: '/sdapi/v1/sd-models',
+		method: 'GET',
+		headers: {'accept': 'application/json'}
+	}
+	return new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
+		const req = http.request(options, (res) => { // обращаемся к серваку
+			let data = "";
+			res.on('data', (d) => {
+				data = data + d;
+			});
+			res.on('end', () => {
+				try {
+					resolve(JSON.parse(data));
+				} catch(e) {
+					console.error(e);
+					resolve(false);
+				}
+			});
+		});
+		req.on('error', (e) => {
+			console.error(e);
 			resolve(false);
 		});
 		req.end();
@@ -85,8 +255,9 @@ const txt2img = async (nek, client, interaction) => {
 	const oldEmbed = interaction.message.embeds[0];
 	const oldComponents = interaction.message.components;
 	const positivePrompts = oldEmbed.fields[0].value.replace(/`/g, '');
-	let negativePrompts = oldEmbed.fields[1].value.replace(/`/g, '');
-	if (!negativePrompts) negativePrompts = "mutation, watermark, bad hands, bad fingers, bad anatomy, crooked fingers, crooked hands, ugly hands, ugly fingers, twisted fingers, non-anatomical fingers";
+	//let negativePrompts = oldEmbed.fields[1].value.replace(/`/g, '');
+	//if (!negativePrompts) 
+	let negativePrompts = "mutation, watermark, bad hands, bad fingers, bad anatomy, crooked fingers, crooked hands, ugly hands, ugly fingers, twisted fingers, non-anatomical fingers";
 	if (!interaction.message.channel.nsfw) negativePrompts = negativePrompts + ", (nsfw, explicit, questionable, pussy, breasts, nipple, areolae, cum, pubic hair, penis:1.1)"
 	const steps = oldEmbed.fields[2].value.replace(/`/g, '')
 	let embed = new Discord.EmbedBuilder()
@@ -164,14 +335,15 @@ const txt2img = async (nek, client, interaction) => {
 		req.end();
 	})
 	clearInterval(timerId); // останавливаем проверку прогресса
+	imgResult.info = JSON.parse(imgResult.info)
 	//console.log(imgResult)
 	const imgBuffer = new Buffer.from(imgResult.images[0], "base64");
 	embed = new Discord.EmbedBuilder()
 		.setTitle('Готово')
 		.setColor(nek.config.basecolor)
 		.setDescription("Ваша кака, сэр")
-		.setImage('attachment://result.png');
-	await interaction.message.edit({ embeds: [embed], files: [{attachment: imgBuffer, name: "result.png"}], components: [] })
+		.setImage('attachment://' + imgResult.info.seed + '.png');
+	await interaction.message.edit({ embeds: [embed], files: [{attachment: imgBuffer, name: imgResult.info.seed + ".png"}], components: [] })
 	workingNow = {timestamp: 0, id: false, step: 0};
 	return;
 }
@@ -197,35 +369,7 @@ const changeModel = async (nek, client, interaction) => {
 		.setColor(nek.config.basecolor)
 		.setDescription("Применение настроек...");
 	await interaction.message.edit({ embeds: [embed], components: []})
-	const options = { // параметры обращения к серваку
-		hostname: PChostname,
-		port: PCport,
-		path: '/sdapi/v1/options',
-		method: 'POST',
-		headers: {}
-	}
-	const optionsRequest = await new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
-		const req = http.request(options, (res) => { // обращаемся к серваку
-			let data = "";
-			res.on('data', (d) => {
-				data = data + d;
-			});
-			res.on('end', () => {
-				try {
-					resolve(JSON.parse(data));
-				} catch(e) {
-					console.error(e);
-					resolve(false);
-				}
-			});
-		});
-		req.on('error', (e) => {
-			console.log(e)
-			resolve(false);
-		});
-		req.write('{"sd_model_checkpoint":"' + interaction.values[0] + '"}'); // пишем данные в сайт
-		req.end();
-	})
+	
 	oldEmbed.data.footer.text = 'Загружена модель ' + interaction.values[0] + '. Можно начинать!'; // заменяем футер
 	oldComponents[1].components[0].data.disabled = false; // включаем кнопку "начать"
 	oldComponents[1].components[0].data.style = 3; // меняем цвет кнопки "начать"
@@ -257,149 +401,148 @@ class Neuro {
         this.desc = "нейромазня";
         this.advdesc = "Это, наверное, самая эксперементальная команда. С помощью компьютера разработчика (если он включён) можно сгенерировать нейромазню.\n" +
 		"Всякие ссылки на то, что помогает этому работать:\n"+
-		"-[stable-diffusion-webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui) - API, с помощью которого можно через бота опросить комп\n"+
-		"-[Anything V5](https://civitai.com/models/9409) - модель AnythingV5\n"+
-		"-[Anything V3](https://civitai.com/models/9409) - модель AnythingV3, совместимая с LoRA\n"+
-		"-[Waifu Diffusion 1.5 beta 2](https://huggingface.co/waifu-diffusion/wd-1-5-beta2) - модель WD15\n";
+		"-[stable-diffusion-webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui) - API, с помощью которого можно через бота опросить комп\n";
         this.name = "neuro";
     }
 
     async run(nek, client, msg, args){
-		args.shift();
 		let embed;
-		
 		if (workingNow.id) {
-			let embed = new Discord.EmbedBuilder()
-				.setTitle('Подождите')
-				.setColor(nek.config.errorcolor)
-				.setDescription('Кто-то уже использует команду. Подождите несколько минут и попробуйте снова');
-			const waitmsg = await msg.reply({ embeds: [embed] }); // запоминаем сообщение
-			return
-		}
-		embed = new Discord.EmbedBuilder()
-			.setTitle('Проверка соединения')
-			.setColor(nek.config.basecolor)
-			.setDescription("Идёт проверка хоста...");
-		const waitmsg = await msg.reply({ embeds: [embed] }); // запоминаем сообщение
-		const options = { // параметры обращения к серваку
-			hostname: PChostname,
-			port: PCport,
-			path: '/sdapi/v1/sd-models',
-			method: 'GET',
-			headers: {}
-		}
-		const sdModels = await new Promise((resolve, reject) => { // ждем пока получим ответ от сайта
-			const req = http.request(options, (res) => { // обращаемся к серваку
-				let data = "";
-				res.on('data', (d) => {
-					data = data + d;
-				});
-				res.on('end', () => {
-					try {
-						const resp = JSON.parse(data);
-						if (!resp.Error) {
-							resolve(resp);
-						} else {
-							resolve(false);
-						}
-					} catch(e) {
-						console.error(e);
-						resolve(false);
-					}
-				});
-			});
-			req.on('error', (e) => {
-				console.log(e);
-				resolve(false);
-			});
-			req.end();
-		})
-		//console.log(sdModels);
-		if (!sdModels) {
 			embed = new Discord.EmbedBuilder()
-				.setTitle('Комп выключен')
+				.setTitle("Подождите")
 				.setColor(nek.config.errorcolor)
-				.setDescription("Хост не отвечает. Попробуйте позже");
-			await waitmsg.edit({ embeds: [embed] }); // запоминаем сообщение
+				.setDescription("Кто-то уже использует команду. Подождите несколько минут и попробуйте снова");
+			await msg.reply({ embeds: [embed] }); // запоминаем сообщение
 			return;
 		}
-		let sdModelsNames = []
-		sdModels.forEach(model => {
-			sdModelsNames.push(model.model_name);
-		});
-		
-		// С этого момента все проверки пройдены, комп отвечает, можно обрабатывать запрос
+		args.shift();
+		let uo = {}; // user options, пользовательские параметры
+		uo.seed = Number(args[0])
+		// вот тут надо обработать кучу аргументов
 		if (!args[0]) {
 			embed = new Discord.EmbedBuilder()
 				.setTitle('Соединение установлено, но делать нечего')
 				.setColor(nek.config.errorcolor)
 				.setDescription("Вы не указали промты ([Что это?](https://strikingloo.github.io/stable-diffusion-vs-dalle-2)). Пока что \"интерактивное заполнение аргументов\" не реализовано.\n"+
 				"Попробуйте `" + nek.config.prefix + this.name + " puppy dog, gray hair` например.");
+			await msg.reply({ embeds: [embed] }); // запоминаем сообщение
+			return;
+		}
+		
+		embed = new Discord.EmbedBuilder()
+			.setTitle("Подключение")
+			.setColor(nek.config.basecolor)
+			.setDescription("Подключение к серверу...");
+		const waitmsg = await msg.reply({ embeds: [embed] }); // запоминаем сообщение
+		
+		const options = await api.getOptions();
+		if (!options) {
+			embed = new Discord.EmbedBuilder()
+				.setTitle("Ошибка")
+				.setColor(nek.config.errorcolor)
+				.setDescription("Хост не отвечает. Попробуйте позже");
 			await waitmsg.edit({ embeds: [embed] }); // запоминаем сообщение
 			return;
 		}
 		
-		const positivePrompts = args.join(' ');
-		const negativePrompts = false;
-		const steps = defSteps;
-		//const NSFW = msg.channel.nsfw;
+		// задаем настроки
+		// если значение не указано ранее, то заменяем их на стандартные
+		uo.steps = 		uo.steps || defaultSettings.steps;
+		uo.sampler = 	uo.sampler || defaultSettings.sampler;
+		uo.cfgScale = 	uo.cfgScale || defaultSettings.cfgScale;
+		
+		uo.width = 		uo.width || defaultSettings.width;
+		uo.height = 	uo.height || defaultSettings.height;
+		
+		uo.seed = 		uo.seed || defaultSettings.seed;
+		uo.denoising = 	uo.denoising || defaultSettings.denoising;
+		uo.clipSkip = 	uo.clipSkip || defaultSettings.clipSkip;
+		
+		const positivePrompts = 'pos';
+		const negativePrompts = 'neg';
 		embed = new Discord.EmbedBuilder()
-			.setTitle('Соединение установлено')
+			.setTitle('Текущие настройки:')
 			.setColor(nek.config.basecolor)
-			.setDescription("Текущие настройки:")
+			//.setDescription("Текущие настройки:")
 			.addFields(
-				{name: 'Положительные промты', value: '`' + positivePrompts + '`'},
-				{name: 'Негативные промты', value: '`' + negativePrompts + '`'},
-				{name: 'Кол-во шагов (steps)', value: '`' + steps + '`'}
+				{name: 'Модель', value: '`' + options.sd_model_checkpoint + '`'},
+				{name: 'Положительные промпты', value: '`' + positivePrompts + '`'},
+				{name: 'Негативные промпты', value: '`' + negativePrompts + '`'},
+				{name: 'Настройки', value: '```\n' + 
+				'Steps: ' + uo.steps + '\n' +
+				'Sampler: ' + uo.sampler + '\n' +
+				'CFG scale: ' + uo.cfgScale + '\n' +
+				'Seed: ' + uo.seed + ' ' + (uo.seed === -1 ? '(random)' : '') + '\n' +
+				'Size: ' + uo.width + 'x' + uo.height + '\n' +
+				'Denoising: ' + uo.denoising + '\n' +
+				'Clip skip: ' + uo.clipSkip + '\n' +
+				'\n```'},
+				//{name: 'Сессия', value: '`' + waitmsg.id + '`'}
 			)
-			.setFooter({text: 'Сейчас выбрана модель ' + sdModelsNames[sdModelsNames.length - 1] + '. Вы можете выбрать другую, но это займёт время'})	
-		const selectList = new Discord.StringSelectMenuBuilder()
-			.setCustomId(msg.author.id + "_0_" + this.name + "_m")
-			.setPlaceholder('Модель')
-		await sdModels.forEach(model => {
-			selectList.addOptions({
-				label: model.model_name,
-				value: model.title
-			});
-		});
-		const startButt = new Discord.ButtonBuilder()
+		// const selectList = new Discord.StringSelectMenuBuilder()
+			// .setCustomId(msg.author.id + "_0_" + this.name + "_m")
+			// .setPlaceholder('Модель')
+		// await sdModels.forEach(model => {
+			// selectList.addOptions({
+				// label: model.model_name,
+				// value: model.title
+			// });
+		// });
+		const startB = new Discord.ButtonBuilder()
 			.setCustomId(msg.author.id + "_0_" + this.name + "_t")
-			.setLabel('Начать')
+			.setLabel('Генерировать')
 			.setStyle(Discord.ButtonStyle.Success)
 			.setDisabled(false)
-		const interruptButt = new Discord.ButtonBuilder()
-			.setCustomId(msg.author.id + "_0_" + this.name + "_i")
-			.setLabel('Прервать')
+			//.setEmoji(':vibration_mode:');
+		const changePromptB = new Discord.ButtonBuilder()
+			.setCustomId(msg.author.id + "_0_" + this.name + "_cp")
+			.setLabel('Изменить промпты')
 			.setStyle(Discord.ButtonStyle.Secondary)
-			.setDisabled(true)
+			.setDisabled(false)
+			//.setEmoji(':gear:');
+		const changeSettingsB = new Discord.ButtonBuilder()
+			.setCustomId(msg.author.id + "_0_" + this.name + "_cs")
+			.setLabel('Изменить настройки')
+			.setStyle(Discord.ButtonStyle.Secondary)
+			.setDisabled(false)
+			//.setEmoji(':gear:');
 
-		const listRow = new Discord.ActionRowBuilder().addComponents(selectList);
-		const buttonsRow = new Discord.ActionRowBuilder().addComponents(startButt, interruptButt);
-		await waitmsg.edit({ embeds: [embed], components: [listRow, buttonsRow] }); // запоминаем сообщение
+		//const listRow = new Discord.ActionRowBuilder().addComponents(selectList);
+		const buttonsRow = new Discord.ActionRowBuilder().addComponents(startB, changePromptB, changeSettingsB);
+		await waitmsg.edit({ embeds: [embed], components: [buttonsRow] }); // запоминаем сообщение
 		return;
 	}
+	
+	
+	
+	
 	async interaction(nek, client, interaction){
-		const customId = interaction.customId.split("_")
-		if (customId[3] === "i"){ // interrupt
-			await interaction.deferUpdate();
-			await interruptTask(nek, client, interaction);
-			return;
+		const customId = interaction.customId.split("_");
+		await interaction.deferUpdate();
+		switch(customId[3]) {
+			case 'i':
+				await api.interrupt();
+				break;
+			case 's':
+				await api.skip();
+				break;
+			case 'cp':
+				await changePrompt(nek, client, interaction);
+				break;
+			case 'cs':
+				await changeSettings(nek, client, interaction);
+				break;
+			case 'm':
+				await changeModel(nek, client, interaction);
+				break;
+			case 'p':
+				await img2img(nek, client, interaction);
+				break;
+			case 't':
+				await txt2img(nek, client, interaction);
+				break;
 		}
-		if (customId[3] === "m"){ // model
-			await interaction.deferUpdate();
-			await changeModel(nek, client, interaction);
-			return;
-		}
-		if (customId[3] === "p"){ // picture
-			await interaction.deferUpdate();
-			await img2img(nek, client, interaction);
-			return;
-		}
-		if (customId[3] === "t"){ // text
-			await interaction.deferUpdate();
-			await txt2img(nek, client, interaction);
-			return;
-		}
+		nek.log(this.name, 'unknown operation', 'red');
 		return;
     }
 }
